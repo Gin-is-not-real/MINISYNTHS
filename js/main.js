9831fff5c0458b3eed3da54556c8ff17057485c1
Gin-is-not-real <ninapariat@live.fr>
2022-01-31 17:41:36 +0100

    Add nav on index for load synths. Add css file for PianoBox

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
-let ROOT = document.querySelector('#root'); 
 ////////////////////////////////////////////////////////////////
 //
+let ROOT = document.querySelector('#root'); 
+
 customElements.define('piano-box', PianoBoxComponent);
-root.appendChild(document.createElement('piano-box'));
+
+let loadPianoBox = document.querySelector('#load-piano-box');
+let loadPianoBoxComponent = document.querySelector('#load-piano-box-component');
+
+let loadButtons = [loadPianoBox, loadPianoBoxComponent];
+
+
+////////////////////////////////////////////////////////////////
+// FUNCTIONS
+function desactiveNavButtons() {
+    loadButtons.forEach(btn => {
+        btn.classList.remove('activ-button');
+    })
+}
+
+////////////////////////////////////////////////////////////////
+// EVENTS LISTENERS
+loadPianoBox.addEventListener('click', function() {
+    desactiveNavButtons();
+    this.classList.add('activ-button');
+
+    ROOT.innerHTML = '<header><h2>Piano Box</h2><p>An autonomous JavaScript class for instanciate a synth with this logic and DOM</p></header>';
+
+    let synth = new PianoBox();
+    ROOT.appendChild(synth.interface);
+})
+
+loadPianoBoxComponent.addEventListener('click', function() {
+    desactiveNavButtons();
+    this.classList.add('activ-button');
+
+    ROOT.innerHTML = '<header><h2>Piano Box Web Component</h2><p>The same synth but here, the class define a Web Component</p></header>';
+
+    ROOT.appendChild(document.createElement('piano-box'));
+})
